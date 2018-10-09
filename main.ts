d8691d61295f1feee7c1bcbb6da176f78d7a7a7d
lixiang <214372150@qq.com>
2018-10-09 15:37:29 +0800

    test

--- main.ts
+++ main.ts
@@ -10,54 +10,26 @@ enum Around {
 namespace MiniMotor {
 
     /**
-     * 设置电机
+     * 设置电机持续运动
      */
-    //% blockId="mini_motor_time" block="%aro|电机 速度%speed| 时长%time 秒"
-    //% speed.min=-1023 speed.max=1023
-    //% weight=100
-    export function motorRun(aro: Around, speed: number, time: number): void {
-
-        if(aro == Around.left){
-            if(speed > 0){
-                pins.analogWritePin(AnalogPin.P13, Math.abs(speed));
+    //% blockId="mini_motor" block="%a|电机 速度%s"
+    //% s.min=-1023 s.max=1023
+    //% weight=99
+    export function motorGo(a: Around, s: number): void {
+        if(a == Around.left){
+            if(s > 0){
+                pins.analogWritePin(AnalogPin.P13, Math.abs(s));
                 pins.digitalWritePin(DigitalPin.P14, 0);
             }else{
-                pins.analogWritePin(AnalogPin.P14, Math.abs(speed));
+                pins.analogWritePin(AnalogPin.P14, Math.abs(s));
                 pins.digitalWritePin(DigitalPin.P13, 0);
             }
         }else{
-            if(speed > 0){
-                pins.analogWritePin(AnalogPin.P15, Math.abs(speed));
+            if(s > 0){
+                pins.analogWritePin(AnalogPin.P15, Math.abs(s));
                 pins.digitalWritePin(DigitalPin.P16, 0);
             }else{
-                pins.analogWritePin(AnalogPin.P16, Math.abs(speed));
-                pins.digitalWritePin(DigitalPin.P15, 0);
-            }
-        }
-
-        //添加时间控制
-        if(time < 0){
-            time = 0;
-        }
-        
-        let time_num = time*1000000;
-
-        control.waitMicros(time_num);
-
-        if(aro == Around.left){
-            if(speed > 0){
-                pins.analogWritePin(AnalogPin.P13, 0);
-                pins.digitalWritePin(DigitalPin.P14, 0);
-            }else{
-                pins.analogWritePin(AnalogPin.P14, 0);
-                pins.digitalWritePin(DigitalPin.P13, 0);
-            }
-        }else{
-            if(speed > 0){
-                pins.analogWritePin(AnalogPin.P15, 0);
-                pins.digitalWritePin(DigitalPin.P16, 0);
-            }else{
-                pins.analogWritePin(AnalogPin.P16, 0);
+                pins.analogWritePin(AnalogPin.P16, Math.abs(s));
                 pins.digitalWritePin(DigitalPin.P15, 0);
             }
         }
