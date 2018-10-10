ae029e88cb9d61e3dff337fde1b257fcf075be69
lixiang <214372150@qq.com>
2018-10-10 11:10:25 +0800

    添加时间控制

--- main.ts
+++ main.ts
@@ -10,6 +10,81 @@ enum Around {
 namespace MiniMotor {
 
     /**
+     * 设置电机
+     */
+    //% blockId="mini_motor_time" block="%aro|电机 速度%speed| 时长%time 秒"
+    //% speed.min=-1023 speed.max=1023
+    //% weight=100
+    export function motorRun(aro: Around, speed: number, time: number): void {
+
+        if (speed >= 1023) {
+            speed = 1023;
+        }
+        if (speed <= -1023) {
+            speed = -1023;
+        }
+
+        if(aro == Around.left){
+            pins.setPull(DigitalPin.P13, PinPullMode.PullUp);
+            pins.setPull(DigitalPin.P14, PinPullMode.PullUp);
+
+            pins.digitalWritePin(DigitalPin.P13, 0);
+            pins.digitalWritePin(DigitalPin.P14, 0);
+
+            if(speed > 0){
+                pins.analogWritePin(AnalogPin.P13, Math.abs(speed));
+                pins.digitalWritePin(DigitalPin.P14, 0);
+            }else{
+                pins.analogWritePin(AnalogPin.P14, Math.abs(speed));
+                pins.digitalWritePin(DigitalPin.P13, 0);
+            }
+        }else{
+            pins.setPull(DigitalPin.P15, PinPullMode.PullUp);
+            pins.setPull(DigitalPin.P16, PinPullMode.PullUp);
+
+            pins.digitalWritePin(DigitalPin.P15, 0);
+            pins.digitalWritePin(DigitalPin.P16, 0);
+
+            if(speed > 0){
+                pins.analogWritePin(AnalogPin.P15, Math.abs(speed));
+                pins.digitalWritePin(DigitalPin.P16, 0);
+            }else{
+                pins.analogWritePin(AnalogPin.P16, Math.abs(speed));
+                pins.digitalWritePin(DigitalPin.P15, 0);
+            }
+        }
+
+        //添加时间控制
+        if(time < 0){
+            time = 0;
+        }
+        
+        let time_num = time*1000;
+
+        basic.pause(time_num);
+
+        if(aro == Around.left){
+            if(speed > 0){
+                pins.analogWritePin(AnalogPin.P13, 0);
+                pins.digitalWritePin(DigitalPin.P14, 0);
+            }else{
+                pins.analogWritePin(AnalogPin.P14, 0);
+                pins.digitalWritePin(DigitalPin.P13, 0);
+            }
+        }else{
+            if(speed > 0){
+                pins.analogWritePin(AnalogPin.P15, 0);
+                pins.digitalWritePin(DigitalPin.P16, 0);
+            }else{
+                pins.analogWritePin(AnalogPin.P16, 0);
+                pins.digitalWritePin(DigitalPin.P15, 0);
+            }
+        }
+        
+    }
+
+
+    /**
      * 设置电机持续运动
      */
     //% blockId="mini_motor" block="%a|电机 速度%s"
